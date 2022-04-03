948c13f88dc0c4b516478e052b2ea94588e5524e
hypermarxeru <102909405+hypermarxeru@users.noreply.github.com>
2022-04-03 12:45:09 -0700

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/WZVTjfT.png?";
+            i.src = "https://i.imgur.com/YzgryNN.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px; transition: opacity 0.2s ease";
             console.log(i);
             var t;
