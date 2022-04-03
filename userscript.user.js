64879bba40b4ec9d1f1ec9b89a18c918c217774c
hypermarxeru <102909405+hypermarxeru@users.noreply.github.com>
2022-04-03 08:26:35 -0700

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  highlight xeno stuff :)
-// @author       hyper, kinda (Credit to Kaguya Discord and Ship for template)
+// @author       hyper, kinda (Credit to Kaguya Discord and Ship for template, this is pretty much copy paste)
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
@@ -13,21 +13,37 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/MGlzs5M.png?";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.src = "https://i.imgur.com/RSqpwIF.png?";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px; transition: opacity 0.2s ease";
             console.log(i);
+            var t;
             function a() {
-              i.style.display = 'none';
-              setTimeout(b, 1000);
+              i.style.opacity = 0;
+              t = setTimeout(b, 1300);
             }
             function b() {
-              i.style.display = null;
-              setTimeout(a, 1000);
+              i.style.opacity = 1;
+              t = setTimeout(a, 1300);
             }
             setInterval(() => {
               i.src += 'a';
             }, 120000)
             b();
+            document.body.addEventListener('keypress', (e) => {
+              if(e.code == 'KeyD') {
+                if(t){
+                  clearTimeout(t);
+                  t = false;
+                }else
+                  b();
+              } 
+            })
+          
+            document.body.addEventListener('keypress', (e) => {
+              if(e.code == 'KeyS') {
+                i.style.opacity = (+i.style.opacity)?0:1;
+              } 
+            })
             return i;
         })())
 
