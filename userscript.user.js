da5b41c217d51ca6c4d60ae1800e7273ee6598e1
hypermarxeru <102909405+hypermarxeru@users.noreply.github.com>
2022-04-03 08:28:45 -0700

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  highlight xeno stuff :)
-// @author       hyper, kinda (Credit to Kaguya Discord and Ship for template, this is pretty much copy paste)
+// @author       hyper, kinda (Credit to Kaguya Discord and Ship for template, this is copy paste)
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
