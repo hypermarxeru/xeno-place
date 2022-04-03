be639eed2aebe58570aca94776b04af7b2ce3d87
hypermarxeru <102909405+hypermarxeru@users.noreply.github.com>
2022-04-03 15:45:33 -0700

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/Zs6HInG.png?";
+            i.src = "https://algoinde.ru/f/1231231.png?";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px; transition: opacity 0.2s ease";
             console.log(i);
             var t;
